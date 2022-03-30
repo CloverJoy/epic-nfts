1310c391e2e33e01200a6b78e21744d95aef0268
Christoffel Angga <Chris.ang.dev@gmail.com>
2022-03-29 17:55:53 -0700

    ERC721 deployment script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,14 +4,11 @@ const main = async () => {
   await nftContract.deployed();
   console.log("Contract deployed to:", nftContract.address);
 
-  // Call the function.
   let txn = await nftContract.makeAnEpicNFT()
-  // Wait for it to be mined.
   await txn.wait()
   console.log("Minted NFT #1")
 
   txn = await nftContract.makeAnEpicNFT()
-  // Wait for it to be mined.
   await txn.wait()
   console.log("Minted NFT #2")
 };
