e99fe32f9db7095db16807ddf3e1699db1639e85
Christoffel Angga <Chris.ang.dev@gmail.com>
2022-03-29 18:40:56 -0700

    Added on chain data

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -10,13 +10,13 @@ contract MyEpicNFT is ERC721URIStorage {
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
 
-  constructor() ERC721 ("MarinNFT", "MARIN") {
+  constructor() ERC721 ("Wen WL Words", "WENWL") {
     console.log("This is my NFT contract, LFG!");
   }
   function makeAnEpicNFT() public {
     uint256 newItemId = _tokenIds.current();
     _safeMint(msg.sender, newItemId);
-    _setTokenURI(newItemId, "https://jsonkeeper.com/b/IKDU");
+    _setTokenURI(newItemId, "data:application/json;base64,ewogICAgIm5hbWUiOiAiUmFuZG9tV29yZHMiLAogICAgImRlc2NyaXB0aW9uIjogIkkgbWVhbiwgaXQncyByYW5kb20gYW5kIGl0J3MgaW1hZ2UiLAogICAgImltYWdlIjogImRhdGE6aW1hZ2Uvc3ZnK3htbDtiYXNlNjQsUEhOMlp5QjRiV3h1Y3owaWFIUjBjRG92TDNkM2R5NTNNeTV2Y21jdk1qQXdNQzl6ZG1jaUlIQnlaWE5sY25abFFYTndaV04wVW1GMGFXODlJbmhOYVc1WlRXbHVJRzFsWlhRaUlIWnBaWGRDYjNnOUlqQWdNQ0F6TlRBZ016VXdJajRLSUNBZ0lEeHpkSGxzWlQ0dVltRnpaU0I3SUdacGJHdzZJSGRvYVhSbE95Qm1iMjUwTFdaaGJXbHNlVG9nYzJWeWFXWTdJR1p2Ym5RdGMybDZaVG9nTVRSd2VEc2dmVHd2YzNSNWJHVStDaUFnSUNBOGNtVmpkQ0IzYVdSMGFEMGlNVEF3SlNJZ2FHVnBaMmgwUFNJeE1EQWxJaUJtYVd4c1BTSmliR0ZqYXlJZ0x6NEtJQ0FnSUR4MFpYaDBJSGc5SWpVd0pTSWdlVDBpTlRBbElpQmpiR0Z6Y3owaVltRnpaU0lnWkc5dGFXNWhiblF0WW1GelpXeHBibVU5SW0xcFpHUnNaU0lnZEdWNGRDMWhibU5vYjNJOUltMXBaR1JzWlNJK1YyVnVWMHhUWlhJOEwzUmxlSFErQ2p3dmMzWm5QZz09Igp9");
     console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
     _tokenIds.increment();
   }
